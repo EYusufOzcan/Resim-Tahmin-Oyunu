b8a079f061700fc2ebbb5efd0e3c0fa33458c521
EYusufOzcan <eyusufozcan@gmail.com>
2016-09-01 20:51:18 +0300

    Add project files.

---
+++ Challenge(ResimTahmin)/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Challenge_ResimTahmin_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbasil = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.btnDegis = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbasil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbasil
+            // 
+            this.pbasil.Location = new System.Drawing.Point(67, 25);
+            this.pbasil.Name = "pbasil";
+            this.pbasil.Size = new System.Drawing.Size(574, 342);
+            this.pbasil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbasil.TabIndex = 0;
+            this.pbasil.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Challenge_ResimTahmin_.Properties.Resources.Desert;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 413);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Challenge_ResimTahmin_.Properties.Resources.Hydrangeas;
+            this.pictureBox2.Location = new System.Drawing.Point(217, 413);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(121, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Challenge_ResimTahmin_.Properties.Resources.Koala;
+            this.pictureBox3.Location = new System.Drawing.Point(379, 413);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(121, 96);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Challenge_ResimTahmin_.Properties.Resources.Lighthouse;
+            this.pictureBox4.Location = new System.Drawing.Point(535, 413);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(121, 96);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(98, 533);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(274, 533);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(434, 533);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(588, 533);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(14, 13);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // btnDegis
+            // 
+            this.btnDegis.Location = new System.Drawing.Point(312, 373);
+            this.btnDegis.Name = "btnDegis";
+            this.btnDegis.Size = new System.Drawing.Size(87, 34);
+            this.btnDegis.TabIndex = 9;
+            this.btnDegis.Text = "Değiştir";
+            this.btnDegis.UseVisualStyleBackColor = true;
+            this.btnDegis.Click += new System.EventHandler(this.btnDegis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 576);
+            this.Controls.Add(this.btnDegis);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbasil);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbasil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbasil;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button btnDegis;
+    }
+}
+
---
+++ Challenge(ResimTahmin)/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Challenge_ResimTahmin_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+        }
+
+        int i = 0, sayac = 1;
+        Random random = new Random();
+        Bitmap[] array = { Properties.Resources.Desert, Properties.Resources.Hydrangeas, Properties.Resources.Koala, Properties.Resources.Lighthouse };
+        ArrayList arraylist = new ArrayList();
+
+        private void btnDegis_Click(object sender, EventArgs e)
+        {
+            PictureBox[] picture = { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
+            RadioButton[] radio = { radioButton1, radioButton2, radioButton3, radioButton4 };
+            int[] location = { 98, 274, 434, 588 };
+
+            for (int z = 0; z < radio.Length; z++)
+            {
+                if (z == 0)
+                {
+                    btnChecked(radio[z], 98);
+                }
+                if (z == 1)
+                {
+                    btnChecked(radio[z], 274);
+                }
+                if (z == 2)
+                {
+                    btnChecked(radio[z], 434);
+                }
+                if (z == 3)
+                {
+                    btnChecked(radio[z], 588);
+                }
+
+            }
+
+            if (i == 0)
+            {
+                MessageBox.Show("Bir tahminde bulunmalısınız.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            int j = random.Next(0, 4);
+            pbasil.Image = array[j];
+            if (i == location[j])
+            {
+                DialogResult result = MessageBox.Show("Tebrikler. " + sayac + ". denemenizde bildiniz." + "\nBir daha oynamak ister misiniz?", "Bilgilendirme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    sayac = 1;
+                    i = 0;
+                    pbasil.Image = null;
+                    btnClear();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                sayac++;
+            }
+
+            for (int k = 0; k < 4;)
+            {
+                int p = random.Next(0, 4);
+                if (!arraylist.Contains(array[p]))
+                {
+                    arraylist.Add(array[p]);
+                    picture[k].Image = array[p];
+                    radio[p].Location = new Point(location[k], 533);
+                    k++;
+                }
+            }
+            arraylist.Clear();
+            btnClear();
+
+        }
+
+        private void btnChecked(RadioButton rdtn, int sayi)
+        {
+            if (rdtn.Checked)
+            {
+                i = sayi;
+            }
+        }
+
+        private void btnClear()
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            i = 0;
+        }
+
+    }
+}
